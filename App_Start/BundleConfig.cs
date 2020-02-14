41218bfd459ec60e18d0f15c8ec063de944c1eab
unknown <thanhhoacth2013@gmail.com>
2020-02-14 11:27:04 +0700

    Angular 9+ASP.NET MVC5

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+namespace WebAngular9mvc5.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/Script/Bundles").Include(
+                      "~/bundles/runtime-es2015.*",
+                       "~/bundles/runtime-es5.*",
+                      "~/bundles/polyfills-es2015.*",
+                       "~/bundles/polyfills-es5.*",
+                      "~/bundles/main-es2015.*",
+                       "~/bundles/main-es5.*"));
+
+            bundles.Add(new StyleBundle("~/Content/Styles").Include(
+                      "~/bundles/style.*"));
+        }
+    }
+}
---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+namespace WebAngular9mvc5.Controllers
+{
+    [Route("api/[controller]")]
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        public string  Get()
+        {
+            return "hoa";
+        }
+    }
+}
